8709b41973e2e36eb9f62fd93fa16319522e89d1
Oleksandr Brudnovskyi <oleksandr.brudnovskyi@customertimes.com>
2026-06-03 22:22:43 +0200

    castle mines styling

--- src/features/castle/building/buildingActions/castleMine/CastleMine.tsx
+++ src/features/castle/building/buildingActions/castleMine/CastleMine.tsx
@@ -1,24 +1,50 @@
+import { classnames } from "#/shared/classnames";
 import { useHistoryStore } from "#/features/history/history.store";
-import css from "../buildingActions.module.css";
+import css from "./castleMine.module.css";
+
+const RESOURCES = [
+  { res: "gold", icon: "/img/resource/gold.webp" },
+  { res: "law", icon: "/img/resource/law.png" },
+  { res: "astrology", icon: "/img/resource/astrology.png" },
+] as const;
 
 const CastleMine = ({ value, buildingID }: CastleMineProps) => {
   const addCastleMines = useHistoryStore((state) => state.addCastleMines);
 
-  const onClick = (res: "gold" | "law" | "astrology") => () => {
-    addCastleMines(buildingID, res, value);
-  };
+  const activeRes = useHistoryStore(
+    (state) => state.castleMines[state.currCastleUUID]?.[buildingID]?.resource,
+  );
+
+  const onClick =
+    (res: "gold" | "law" | "astrology") =>
+    (e: React.MouseEvent<HTMLButtonElement>) => {
+      addCastleMines(buildingID, res, value);
+      e.stopPropagation();
+    };
 
   return (
     <div className={css.resources}>
-      <button type="button" onClick={onClick("gold")}>
-        gold
-      </button>
-      <button type="button" onClick={onClick("law")}>
-        law
-      </button>
-      <button type="button" onClick={onClick("astrology")}>
-        astrology
-      </button>
+      {RESOURCES.map(({ res, icon }) => {
+        const isActive = activeRes === res;
+
+        const className = classnames({
+          [css.option]: true,
+          [css.active]: isActive,
+          [css.inactive]: !isActive,
+        });
+
+        return (
+          <button
+            key={res}
+            type="button"
+            className={className}
+            onClick={onClick(res)}
+            title={res}
+          >
+            <img className={css.icon} src={icon} alt={res} />
+          </button>
+        );
+      })}
     </div>
   );
 };
