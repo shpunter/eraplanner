354a45cee78716bb59786308130f3f2425a94675
Oleksandr Brudnovskyi <oleksandr.brudnovskyi@customertimes.com>
2026-06-18 12:50:38 +0200

    add faction change

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,10 @@ import { createFileRoute, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
   beforeLoad: () => {
-    throw redirect({ to: "/faction/$id", params: { id: "temple" }, search: { m: "castles" } });
+    throw redirect({
+      to: "/faction/$id",
+      params: { id: "temple" },
+      search: { m: "castles" },
+    });
   },
 });
