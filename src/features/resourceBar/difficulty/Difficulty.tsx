176c8aa530e9d5f5cec7218f07ee7c3bb638b433
Oleksandr Brudnovskyi <oleksandr.brudnovskyi@customertimes.com>
2026-07-03 10:04:27 +0200

    move difficalty change to the popup

--- src/features/resourceBar/difficulty/Difficulty.tsx
+++ src/features/resourceBar/difficulty/Difficulty.tsx
@@ -1,29 +1,44 @@
 import { useHistoryStore } from "../../history/history.store";
-import { classnames } from "#/shared/classnames";
+import Popover from "#/components/popover/Popover";
 import css from "./difficulty.module.css";
+import { classnames } from "#/shared/classnames";
 
-const difficulties = ["♟", "♞", "♝", "♜", "♛", "♚"];
+const difficulties = ["♟", "♞", "♝", "♜", "♛", "♚"] as const;
 
 const Difficulty = () => {
   const difficulty = useHistoryStore((state) => state.difficulty);
   const setDifficulty = useHistoryStore((state) => state.setDifficulty);
 
   return (
-    <div className={css.difficulties}>
-      {difficulties.map((label, idx) => {
-        const onClick = () => setDifficulty(idx as 0 | 1 | 2 | 3 | 4 | 5);
-        const classNames = classnames({
-          [css.difficulty]: true,
-          [css.active]: difficulty === idx,
-        });
+    <Popover>
+      <Popover.Trigger className={css.difficulty}>
+        {difficulties[difficulty]}
+      </Popover.Trigger>
 
-        return (
-          <div key={label} onClick={onClick} className={classNames}>
-            {label}
+      <Popover.Content>
+        {({ close }) => (
+          <div className={css.list}>
+            {difficulties.map((label, idx) => {
+              const className = classnames({
+                [css.option]: true,
+                [css.active]: difficulty === idx,
+              });
+
+              const onClick = () => {
+                setDifficulty(idx as 0 | 1 | 2 | 3 | 4 | 5);
+                close();
+              };
+
+              return (
+                <div key={label} className={className} onClick={onClick}>
+                  {label}
+                </div>
+              );
+            })}
           </div>
-        );
-      })}
-    </div>
+        )}
+      </Popover.Content>
+    </Popover>
   );
 };
 
